64391954a1341191bea8b35a7b5d4c074f272065
Nand Kishor Vyas <86528339+nand2509@users.noreply.github.com>
2025-05-22 22:15:40 +0530

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-import google.generativeai as genai #---->
-genai.configure(api_key="AIzaSyBwrxR_jXcVCczEKom-nYUCPqvx9EReHuo")
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Who is Modi?")
-print(response.text)
